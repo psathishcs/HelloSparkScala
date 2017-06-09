fea6315893dbaba5a2306aa43476e234333e6110
SathishKumarParthasarathy <p.sathish.cs@gmail.com>
2017-06-09 18:53:52 -0500

    processing number in Spark RDD

--- src/main/scala/org/hello/HelloSpark.scala
+++ src/main/scala/org/hello/HelloSpark.scala
@@ -8,6 +8,11 @@ object HelloSpark {
   def main(args: Array[String]){
     val conf = new SparkConf().setAppName("HelloSparkScala").setMaster("spark://hadoop.master.com:7077")
     val sc = new SparkContext(conf)
+    
+    val nums = sc.parallelize(List(1,2,3,5,6,7,8,20,235,366,3))
+    val result = nums.map(x=> x*x)
+    println(result.collect().mkString(" ,"))
+    
     val textfile = sc.textFile("hdfs://hadoop.master.com:9000/user/psathishcs/Input/Books/The_Outline_of_Science.txt")
     println("No of Lines -> " + textfile.count())
     println("Completed....");
