89b138bf074b23555b0b24b94c3704f467fe1a17
SathishKumarParthasarathy <p.sathish.cs@gmail.com>
2017-09-11 18:21:18 -0500

    filter

--- src/main/scala/org/cca175/Problem1.scala
+++ src/main/scala/org/cca175/Problem1.scala
@@ -2,6 +2,21 @@ package org.cca175
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.{SparkContext, SparkConf}
 import com.databricks.spark.avro._
+import org.apache.spark.sql.functions._;
+/**
+ * root
+ |-- order_id: integer (nullable = true)
+ |-- order_date: long (nullable = true)
+ |-- order_customer_id: integer (nullable = true)
+ |-- order_status: string (nullable = true)
+ |-- order_item_id: integer (nullable = true)
+ |-- order_item_order_id: integer (nullable = true)
+ |-- order_item_product_id: integer (nullable = true)
+ |-- order_item_quantity: integer (nullable = true)
+ |-- order_item_subtotal: float (nullable = true)
+ |-- order_item_product_price: float (nullable = true)
+ * 
+ */
 object Problem1 {
   def main(args : Array[String]) {
     val conf = new SparkConf().setAppName("FilterSparkRDD - Scala")
@@ -19,5 +34,10 @@ object Problem1 {
     println("joinedOrderDF Count -> " + joinedOrderDF.count());
     println("joinedOrderDF first -> " + joinedOrderDF.first());
     joinedOrderDF.printSchema();
+    joinedOrderDF.show();
+    joinedOrderDF.select("order_status").show();
+    println("Filter........................>");
+    joinedOrderDF.filter(joinedOrderDF("order_customer_id") === 2755).show();
+        
   }
 }
